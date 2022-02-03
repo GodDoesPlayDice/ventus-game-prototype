80268968185ab3f7e7897d486f4a06456f5abea4
GodDoesPlayDice <alik_okroyan@mail.ru>
2022-02-03 15:59:12 +0300

    attack radius controller on character prefab

---
+++ Assets/Scripts/Actors/AttackRadiusController.cs
@@ -0,0 +1,58 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Actors
+{
+    public class AttackRadiusController : MonoBehaviour
+    {
+        public float attackRadius;
+        public float lineThickness;
+        public float growSmooth = 0.01f;
+        public float shrinkSmooth = 0.02f;
+        public bool isEnabled = false;
+        
+        public Material material;
+        private static readonly int LineThickness = Shader.PropertyToID("_Line_Thickness");
+        private static readonly int Radius = Shader.PropertyToID("_Radius");
+
+        private void Start()
+        {
+            if (material != null)
+            {
+                material.SetFloat(LineThickness, lineThickness);
+            }
+            else
+            {
+                Debug.LogWarning("No material on attack radius object!", this);
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if (material == null) return;
+            float currentRadius = material.GetFloat(Radius);
+            if (isEnabled)
+            {
+                if (attackRadius - currentRadius >= 0.01f)
+                {
+                    material.SetFloat(Radius, Mathf.Lerp(currentRadius, attackRadius, growSmooth));
+                }
+            }
+            else
+            {
+                if (currentRadius >= 0.01f)
+                {
+                    material.SetFloat(Radius, Mathf.Lerp(currentRadius, 0, shrinkSmooth));
+                }
+            }
+        }
+
+        public void ShowAttackRadius()
+        {
+            
+        }
+        
+    }
+}
