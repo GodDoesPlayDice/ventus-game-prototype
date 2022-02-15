31eb63909af38a175645a0cd20e2a205811b3b18
GodDoesPlayDice <alik_okroyan@mail.ru>
2022-02-15 15:11:03 +0300

    new armour selection screen and logic

--- Assets/Scripts/UI/ArmorSelector.cs
+++ Assets/Scripts/UI/ArmorSelector.cs
@@ -1,6 +1,9 @@
-﻿using Unity.VisualScripting;
+﻿using System;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace UI
 {
@@ -9,13 +12,59 @@ namespace UI
         public LoadSceneEvent loadSceneEvent;
         public SceneEnum sceneToOpen;
 
+        public Image baseLayerOnCharacterView;
+        public Image armourLayerOnCharacterView;
+        public GameObject startGameButton;
+
+        public TextMeshProUGUI healthValue;
+        public TextMeshProUGUI staminaValue;
+        public TextMeshProUGUI strengthValue;
+        public TextMeshProUGUI luckValue;
+        
+        private GameObject _selectedArmour;
+
+        private void Start()
+        {
+            startGameButton.SetActive(false);
+        }
+
         public void SelectArmor(GameObject obj)
         {
-            SaveSelectedItem(obj);
-            
+            var armourSprite = obj.GetComponent<Image>();
+            var armourButtonController = obj.GetComponent<ArmourButtonController>();
+            UpdateCharacterView(armourSprite.sprite, armourButtonController.isDisablingBaseLayer);
+            UpdateStatsView(armourButtonController);
+
+            _selectedArmour = obj;
+
+            if (!startGameButton.activeSelf)
+            {
+                startGameButton.SetActive(true);
+            }
+        }
+
+        public void OnStartGameButtonPressed()
+        {
+            if (_selectedArmour == null) return;
+            SaveSelectedItem(_selectedArmour);
+
             var sceneEP = new LoadSceneEP(sceneToOpen,
                 SceneEnum.TITLE, true, null, null, true);
             loadSceneEvent.Raise(sceneEP);
+        }
+
+        private void UpdateCharacterView(Sprite armourSprite, bool isDisablingBaseLayer)
+        {
+            armourLayerOnCharacterView.sprite = armourSprite;
+            baseLayerOnCharacterView.gameObject.SetActive(!isDisablingBaseLayer);
+        }
+
+        private void UpdateStatsView(ArmourButtonController armourButtonController)
+        {
+            healthValue.text = armourButtonController.health;
+            staminaValue.text = armourButtonController.stamina;
+            strengthValue.text = armourButtonController.strength;
+            luckValue.text = armourButtonController.luck;
         }
 
         private void SaveSelectedItem(GameObject obj)
@@ -28,6 +77,7 @@ namespace UI
                     Destroy(it);
                 }
             }
+
             DontDestroyOnLoad(Instantiate(obj));
         }
     }
---
+++ Assets/Scripts/UI/ArmourButtonController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace UI
+{
+    public class ArmourButtonController : MonoBehaviour
+    {
+        public bool isDisablingBaseLayer = false;
+        
+        // TEMP
+        public string health = "100";
+        public string stamina = "100";
+        public string strength = "100";
+        public string luck = "100";
+    }
+}
